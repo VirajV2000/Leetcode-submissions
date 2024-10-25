4262a5ba9135cac6019bf1d7c2d523477d92f13f
Viraj <121506903+VirajV2000@users.noreply.github.com>
2024-10-25 16:18:30 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42 MB (77.12%)

---
+++ my-folder/1146-greatest-common-divisor-of-strings/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if(str2.isEmpty()){
+            return str1;
+        }else if(str2.length()>str1.length()){
+            return gcdOfStrings(str2,str1);
+        }else if(!str1.startsWith(str2)){
+            return "";
+        }else{
+            return gcdOfStrings(str1.substring(str2.length()),str2);
+        }
+    }
+}
